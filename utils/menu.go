454fdec61a196b48a09168867f24c4d73f0a336b
105hua <joshuajtudball2003@gmail.com>
2026-01-31 16:38:21 +0000

    Reworked the title to look a little nicer.

--- utils/menu.go
+++ utils/menu.go
@@ -3,16 +3,20 @@ package utils
 import (
 	"fmt"
 	"strings"
+
+	"github.com/fatih/color"
 )
 
 func DisplayTitle() {
-	fmt.Println(strings.Repeat("─", 100))
-	// fmt.Println("")
-	fmt.Println("     █")
-	fmt.Println("   █████")
-	fmt.Println("   █ █ █      Programming CLI | v0.0.1")
-	fmt.Println("   █████")
-	fmt.Println("   █ █ █")
-	// fmt.Println("")
-	fmt.Println(strings.Repeat("─", 100))
+	robotColor := color.New(color.FgHiYellow).SprintFunc()
+	lineColor := color.New(color.FgYellow).SprintFunc()
+	boldText := color.New(color.Bold).SprintFunc()
+
+	fmt.Println(lineColor(strings.Repeat("─", 75)))
+	fmt.Println(robotColor("     █"))
+	fmt.Println(robotColor("   █████"))
+	fmt.Println(robotColor("   █ █ █      ") + boldText("Programming CLI | v0.0.1"))
+	fmt.Println(robotColor("   █████"))
+	fmt.Println(robotColor("   █ █ █"))
+	fmt.Println(lineColor(strings.Repeat("─", 75)))
 }
